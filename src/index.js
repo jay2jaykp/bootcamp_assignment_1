6dbefc6571f2f354de7fc078ba16d1b06b1e69a5
Nidhi Patel <nidhipatel439@gmail.com>
2022-11-05 18:20:10 -0400

    2nd lesson

--- src/index.js
+++ src/index.js
@@ -1,51 +1,140 @@
-class Employee {
-  constructor(name, id, email) {
-    this.name = name;
-    this.id = id;
-    this.email = email;
-  }
-  // methods
-  getName() {
-    return this.name;
-  }
+import fs from "fs";
+import { Manager } from "./myClass.js";
 
-  getId() {
-    return this.id;
-  }
+import inquirer from "inquirer";
 
-  getEmail() {
-    return this.email;
-  }
+// global variable
+// declaration , no assignment
+let myManager;
 
-  getRole() {
-    return "Employee";
+async function whatNext() {
+  // this function should ask user what they want to do next
+  // provide three options 1,2,3
+  // based on their answers, we call different functions
+  const options = await inquirer.prompt([
+    {
+      message: `What do you want to do next?
+    [1] Add Engineer
+    [2] Add Intern
+    [3] Finish Team
+    `,
+      name: "option",
+    },
+  ]);
+
+  // question to user
+
+  // user answers 1,2,3
+
+  if (options.option === 1) {
+    // condition if user put 1
+    AddEngineer();
+  } else if (options.option === 2) {
+    // if user put 2
+    AddIntern();
+  } else {
+    // if user put 3
+    finishTeam();
   }
 }
 
-class Manager extends Employee {
-  constructor(name, id, email, officeNumber) {
-    super(name, id, email);
-    this.officeNumber = officeNumber;
-  }
-  getRole() {
-    return "Manager";
-  }
+// create a function that takes in Manager's input
+async function AddManager() {
+  // asking 4 question one by one
+  const managerName = await inquirer.prompt([
+    {
+      message: "What is your name?",
+      name: "name",
+    },
+  ]);
+
+  const managerId = await inquirer.prompt([
+    {
+      message: "What is your employeeID?",
+      name: "id",
+    },
+  ]);
+
+  const managerEmail = await inquirer.prompt([
+    {
+      message: "What is your email?",
+      name: "email",
+    },
+  ]);
+
+  const officeNumber = await inquirer.prompt([
+    {
+      message: "What is your office number?",
+      name: "office_number",
+    },
+  ]);
+
+  // assign these values to Class Manager
+  // instanciating Manager class
+  // save user input to global variable so that we can use them in our finishTeam function
+  myManager = new Manager(
+    managerName.name,
+    managerId.id,
+    managerEmail.email,
+    officeNumber.office_number
+  );
+
+  // call function that gives three options to user
+  whatNext();
 }
 
-class Engineer extends Employee {
-  constructor(name, id, email, github) {
-    super(name, id, email);
-    this.github = github;
-  }
-  getRole() {
-    return "Engineer";
-  }
-  getGithub() {
-    return this.github;
-  }
+async function AddEngineer() {
+  // ask questions to add information about engineer
+
+  // save engineer information into global variable
+
+  // what next
+  whatNext();
 }
 
-// an instance of class Employee
-const MrJ = new Engineer("Jay", 1, "test@gmail.com", 123456);
+async function AddIntern() {
+  //  ask intern's information
 
-console.log(MrJ.github);
+  // save it globally
+
+  // what next
+  whatNext();
+}
+
+async function finishTeam() {
+  // generate HTML file using values provided by the user
+  fs.writeFile(
+    "./dist/index.html",
+    `<!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <
+          <title>Document</title>
+        </head>
+        <body>
+          <h1 class="title">${myManager.name}</h1>
+          <h2>${myManager.id}</h2>
+          <h2>${myManager.email}</h2>
+          <h2>${myManager.officeNumber}</h2>
+        </body>
+      </html>
+      `,
+    function (err) {
+      if (err) throw err;
+      console.log("File is created successfully.");
+    }
+  );
+
+  // finish
+  console.log("html file is ready in ./dist/index.html");
+}
+
+// call this function
+AddManager();
+
+// two types of functions
+// 1. function that return SOMETHING
+// 2. function that return NOTHING || void functions
--- src/math.js
+++
@@ -1,9 +0,0 @@
-function squaredValue(num) {
-  // multiple oprations
-  const x = num + 2;
-  const y = x + 4;
-
-  return y;
-}
-
-module.exports = { squaredValue };
